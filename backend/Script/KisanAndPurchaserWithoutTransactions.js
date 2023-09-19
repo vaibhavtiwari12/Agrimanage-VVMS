8290e2c35b5fa51b17061033d8f14ec5d59448fa
Vaibhav Tiwari <tvaibhav1204@gmail.com>
2023-09-19 17:42:46 +0530

    Added script to copy purchaser and Kisans

---
+++ backend/Script/KisanAndPurchaserWithoutTransactions.js
@@ -0,0 +1,60 @@
+
+const { MongoClient } = require("mongodb");
+const uri = ""//Connnetion String;
+const options = { useUnifiedTopology: true };
+const duplicateKisanDataWithoutTransactions = async (req) => {
+    const client = new MongoClient(uri, options);
+    await client.connect();
+    const db = client.db("VVMS");
+    let kisans = [];
+    kisans = await db
+      .collection("kisans")
+      .find({}).toArray();
+      
+
+    const kisansWithoutTransactions = kisans.map(kisan => {
+        delete kisan.transactions;
+        kisan.balance = 0;
+        kisan.carryForwardAmount = 0;
+        return kisan;
+    })
+    
+    
+        await db
+      .collection("kisans2023")
+      .insertMany(kisansWithoutTransactions);
+      await client.close()
+
+
+    console.log("Kisan Without Transactions ",  kisansWithoutTransactions)
+
+};
+
+const duplicatePurchaserDataWithoutTransactions =async  () => {
+    const client = new MongoClient(uri, options);
+    await client.connect();
+    const db = client.db("VVMS");
+    let purchasers = [];
+    purchasers = await db
+      .collection("purchasers")
+      .find({}).toArray();
+      
+
+    const purchaserssWithoutTransactions = purchasers.map(purchaser => {
+        delete purchaser.transactions;
+        purchaser.balance = 0;
+        return purchaser;
+    })
+    
+    
+        await db
+      .collection("purchasers2023")
+      .insertMany(purchaserssWithoutTransactions);
+      await client.close()
+
+
+    console.log("Purcahsers Without Transactions ",  purchaserssWithoutTransactions)
+}
+
+duplicateKisanDataWithoutTransactions();
+duplicatePurchaserDataWithoutTransactions()
