733b321f0ede98474f41cf9a38a06a66ee426a54
VT <vt@MacBook-Air.local>
2025-12-07 11:55:07 +0530

    config: allow warnings in linting, block only on errors

--- frontend/src/Store/configureStore.js
+++ frontend/src/Store/configureStore.js
@@ -1,9 +1,8 @@
-import { createBrowserHistory } from 'history'
-import { applyMiddleware, compose, createStore } from 'redux'
-import { routerMiddleware } from 'connected-react-router'
-import createRootReducer from './reducers'
-...
-export const history = createBrowserHistory()
+import { createBrowserHistory } from 'history';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { routerMiddleware } from 'connected-react-router';
+import createRootReducer from './reducers';
+export const history = createBrowserHistory();
 
 export default function configureStore(preloadedState) {
   const store = createStore(
@@ -11,11 +10,11 @@ export default function configureStore(preloadedState) {
     preloadedState,
     compose(
       applyMiddleware(
-        routerMiddleware(history), // for dispatching history actions
+        routerMiddleware(history) // for dispatching history actions
         // ... other middlewares ...
-      ),
-    ),
-  )
+      )
+    )
+  );
 
-  return store
+  return store;
 }
